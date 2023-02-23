649cea5c2c2ede3eba08a81ffa7906ff43f0c1c7
eps1lon <silbermann.sebastian@gmail.com>
2023-02-23 20:21:49 +0100

    Initial commit

---
+++ automatic.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.element = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
+exports.element = (0, jsx_runtime_1.jsx)("div", {});
